169262722080d3b7bdbe3c42cbc6be177fb60c19
Robbie <ghosthotdog@hotmail.com>
2018-07-26 16:45:30 +0300

    style-types

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 2017,
+    jsx: true
+  },
+
+  extends: [
+    require.resolve("eslint-config-cup"),
+    require.resolve("eslint-config-cup-recommended")
+  ],
+
+  plugins: ["eslint-plugin-prettier", "eslint-plugin-flowtype"],
+
+  rules: {
+    "flowtype/define-flow-type": 1,
+
+    "prettier/prettier": [
+      "error",
+      {
+        useTabs: false,
+        printWidth: 80,
+        tabWidth: 2,
+        singleQuote: false,
+        trailingComma: "all",
+        bracketSpacing: false,
+        jsxBracketSameLine: false,
+        parser: "babylon",
+        semi: true
+      }
+    ]
+  }
+};
